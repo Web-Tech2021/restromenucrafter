fbd6ce62a837bfae2ea74168c80627e2d1a4ad52
Nirmala9955 <nirmalakumarsahu7@gmail.com>
2023-09-13 07:13:17 +0530

    change the things

--- restromenucrafter/src/main/sql/release_1.sql
+++ restromenucrafter/src/main/sql/release_1.sql
@@ -1,6 +1,8 @@
+CREATE DATABASE  IF NOT EXISTS `restromenucrafter` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `restromenucrafter`;
 -- MySQL dump 10.13  Distrib 8.0.31, for Win64 (x86_64)
 --
--- Host: 127.0.0.1    Database: restromenucrafters
+-- Host: 127.0.0.1    Database: restromenucrafter
 -- ------------------------------------------------------
 -- Server version	5.7.40-log
 
@@ -166,4 +168,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-09-13  6:09:12
+-- Dump completed on 2023-09-13  7:09:44
